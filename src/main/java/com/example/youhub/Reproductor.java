3d21a2852d97e68a73dee70c19529272d2024d61
Wolfban <estebanbarrera104@gmail.com>
2022-07-29 07:31:26 -0600

    Se agregan 2 FXML y el reproductor se enlaza con su fxml correspondiente

---
+++ src/main/java/com/example/youhub/Reproductor.java
@@ -0,0 +1,72 @@
+package com.example.youhub;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.media.MediaView;
+
+/**
+ * @author Esteban Barrera
+ */
+public class Reproductor {
+
+    @FXML
+    private HBox Hbox;
+
+    @FXML
+    private Label PantallaCompleta;
+
+    @FXML
+    private Label TiempoTotal;
+
+    @FXML
+    private Label TiempoVid;
+
+    @FXML
+    private Label Velocidad;
+    @FXML
+    private MediaView Vid;
+
+
+    @FXML
+    private Slider VidDuracion;
+
+    @FXML
+    private HBox Volumen;
+
+    @FXML
+    private Slider Volumenn;
+
+    @FXML
+    private Button btnPlayStop;
+
+    @FXML
+    private Button btonAgregarALista;
+
+    @FXML
+    private Label lblVolumen;
+
+    @FXML
+    private AnchorPane scr;
+
+    @FXML
+    private VBox vbox;
+
+    @FXML
+    void Click(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click(ActionEvent event) {
+
+    }
+
+}
+
+
