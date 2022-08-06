140055128d121e60cb0ef54d4ff95a38bdeb857a
Wolfban <estebanbarrera104@gmail.com>
2022-08-06 04:27:03 -0600

    Se agrega El Log in y el registrar

---
+++ src/main/java/com/example/youhub/Login.java
@@ -0,0 +1,36 @@
+package com.example.youhub;
+
+/**
+ * @author Esteban Barrera
+ */
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Login {
+
+    @FXML
+    private TextField TxtContrasenna;
+
+    @FXML
+    private Button btnIniciar;
+
+    @FXML
+    private Button btnRegistrarse;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    void Click(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click(ActionEvent event) {
+
+    }
+
+}
---
+++ src/main/java/com/example/youhub/Register.java
@@ -0,0 +1,60 @@
+package com.example.youhub;
+
+/**
+ * @author Esteban Barrera
+ */
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Register implements Initializable {
+
+    @FXML
+    private Button btnLogin;
+
+    @FXML
+    private Button btnRegistrar;
+
+    @FXML
+    private TextField txtApellido1;
+
+    @FXML
+    private TextField txtApellido2;
+
+    @FXML
+    private TextField txtConfirContrasenna;
+
+    @FXML
+    private TextField txtContrasenna;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private TextField txtID;
+
+    @FXML
+    private TextField txtNombre;
+
+
+
+    @FXML
+    void Click(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click(ActionEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
