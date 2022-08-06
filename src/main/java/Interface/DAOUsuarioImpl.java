00401c1e945db387303453b43460b67d4c71d1e3
IGNACIO JOSUE ZELADA ARAYA <izeladaa@ucenfotec.ac.cr>
2022-08-06 06:06:43 -0600

    Se crea el proceso para annadir un usuario que se esta registrando en la tabal de usuarios

--- src/main/java/Interface/DAOUsuarioImpl.java
+++ src/main/java/Interface/DAOUsuarioImpl.java
@@ -8,4 +8,19 @@ import java.sql.PreparedStatement;
 
 public class DAOUsuarioImpl extends Conexion implements DAOUsuario {
 
+    @Override
+    public void registrar(Usuarios u) throws Exception {
+        try {
+            this.openConnection();
+            PreparedStatement st = this.conexion.prepareStatement("INSERT INTO usuario(nombreusuario, correousuario, contrasennausuario) VALUES(?)");
+            st.setString(1, u.getNombre());
+            st.setString(1, u.getCorreo());
+            st.setString(1, u.getContrasenna());
+            st.executeUpdate();
+        }catch(Exception e){
+            throw e;
+        } finally {
+            this.closeConnection();
+        }
+    }
 }
