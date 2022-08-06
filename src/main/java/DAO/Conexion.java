6dc2c6269654de4d457898464bf6387ab2f87cda
IGNACIO JOSUE ZELADA ARAYA <izeladaa@ucenfotec.ac.cr>
2022-08-06 05:44:08 -0600

    Se crean los procesos de conexion a la base de datos

--- src/main/java/DAO/Conexion.java
+++ src/main/java/DAO/Conexion.java
@@ -1,4 +1,35 @@
 package DAO;
 
+import java.sql.*;
+
 public class Conexion {
+
+    private Connection conexion;
+
+    private static Conexion instance;
+
+    private Conexion (){
+
+    }
+
+    public static Conexion getInstance(){
+        if(instance==null){
+            instance = new Conexion();
+        }
+
+        return instance;
+    }
+
+    public Connection openConnection() throws SQLException {
+        closeConnection();
+        this.conexion= (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/instance","root","Pokemon.2012");
+
+        return this.conexion;
+    }
+
+    public void closeConnection() throws SQLException{
+        if(this.conexion!=null && !this.conexion.isClosed()){
+            this.conexion.close();
+        }
+    }
 }
