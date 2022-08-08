fd8185dab3b0fe0994c66a2aab824f51e09949a3
lemonherbtea <isaacmoralescr@gmail.com>
2022-08-07 23:55:02 -0600

    Documentación de Conexion.java agregada

--- src/main/java/DAO/Conexion.java
+++ src/main/java/DAO/Conexion.java
@@ -12,6 +12,10 @@ public class Conexion {
 
     }
 
+    /** Esta función genera una instancia que debe de preparar una conexión entre la aplicación y la base de datos
+     * @author Ignacio Josué Zelada Araya
+     * @return devuelve una nueva conexión. En el caso de que instance sea nulo, crea una nueva oportunidad de conexión.
+     */
     public static Conexion getInstance(){
         if(instance==null){
             instance = new Conexion();
@@ -20,6 +24,12 @@ public class Conexion {
         return instance;
     }
 
+    /**Esta función aprovecha la instancia generada por la función anterior para abrir una conexión entre la aplicación y la base de datos
+     *@author Ignacio Josue Zelada ARaya
+
+     * @return La conexión creada para enviar y recibir datos entre la aplicación y la base de datos
+     * @throws SQLException
+     */
     public Connection openConnection() throws SQLException {
         closeConnection();
         this.conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/YouHub","root","Pokemon.2012");
