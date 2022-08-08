8c9c476e5d380dc684df8e255503fe4fd3afef1d
IGNACIO JOSUE ZELADA ARAYA <izeladaa@ucenfotec.ac.cr>
2022-08-07 18:48:50 -0600

    Se modifica el module info para que acceda al modelo de videos

--- src/main/java/com/example/youhub/BuscadorVideos.java
+++ src/main/java/com/example/youhub/BuscadorVideos.java
@@ -1,5 +1,6 @@
 package com.example.youhub;
 
+import DAO.DAOVideo;
 import Modelo.Videos;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -8,11 +9,16 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.net.URL;
+import java.sql.SQLException;
+import java.util.PropertyPermission;
 import java.util.ResourceBundle;
 
 public class BuscadorVideos implements Initializable {
+
+    DAOVideo daoVideo= new DAOVideo();
 
     @FXML
     private TextField searchBar;
@@ -35,11 +41,24 @@ public class BuscadorVideos implements Initializable {
     @FXML
     private TableColumn<Videos, String> ubicacionVideo;
 
-    ObservableList<Videos> ListaVideos = FXCollections.observableArrayList();
+    ObservableList<Videos> listaVideos = FXCollections.observableArrayList();
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        try {
+            listaVideos.addAll(daoVideo.listarVideos());
 
+            nombreVideo.setCellValueFactory(new PropertyValueFactory<>("nombre"));
+            categoriaVideo.setCellValueFactory(new PropertyValueFactory<>("categoria"));
+            descripcionVideo.setCellValueFactory(new PropertyValueFactory<>("descripcion"));
+            fechaSubidoVideo.setCellValueFactory(new PropertyValueFactory<>("fechaSubido"));
+            ubicacionVideo.setCellValueFactory(new PropertyValueFactory<>("ubicacionLocal"));
+
+            tablaVideos.setItems(listaVideos);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
